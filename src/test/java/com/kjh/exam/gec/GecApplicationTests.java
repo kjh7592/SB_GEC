2466a9c36fa638ddf812c3b15b79c39481b578d5
kjh7592 <sajh0405@gmail.com>
2022-12-21 12:41:39 +0900

    환경 세팅

---
+++ src/test/java/com/kjh/exam/gec/GecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjh.exam.gec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

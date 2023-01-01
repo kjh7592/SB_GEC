5c61133bab48eb391712004cc72ef601aae8dc5d
kjh7592 <sajh0405@gmail.com>
2023-01-01 11:07:27 +0900

    NeedLoginInterceptor 구현. registry 등록. 로그인 체크를 앞단에서 실행

--- src/main/java/com/kjh/exam/gec/vo/Rq.java
+++ src/main/java/com/kjh/exam/gec/vo/Rq.java
@@ -1,7 +1,12 @@
 package com.kjh.exam.gec.vo;
 
+import java.io.IOException;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+
+import com.kjh.exam.gec.util.Utility;
 
 import lombok.Getter;
 
@@ -10,9 +15,13 @@ import lombok.Getter;
 public class Rq {
 	@Getter
 	private int loginedMemberId;
-
-	public Rq(HttpServletRequest req) {
-
+	private HttpServletRequest req;
+	private HttpServletResponse resp;
+	
+	public Rq(HttpServletRequest req, HttpServletResponse resp) {
+		this.req = req;
+		this.resp = resp;
+		
 		HttpSession httpSession = req.getSession();
 
 		int loginedMemberId = 0;
@@ -24,4 +33,29 @@ public class Rq {
 		this.loginedMemberId = loginedMemberId;
 	}
 
+	public void jsPrintHistoryBack(String msg) throws IOException{
+		resp.setContentType("text/html; charset=UTF-8");
+
+		println("<script>");
+
+		if(!Utility.empty(msg)) {
+			println("alert('" + msg + "');");
+		}
+
+		println("history.back();");
+		println("</script>");
+	}
+
+	private void print(String str) {
+		try {
+			resp.getWriter().append(str);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void println(String str) {
+		print(str + "\n");
+	}
+	
 }
